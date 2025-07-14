5f815f2568909faac0ce7ad0207ab538f40396c9
Ido Ben-Hur <idoybh2@gmail.com>
2025-07-14 11:38:52 +0300

    config: Enable Bluetooth auto turn on by default
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,8 +1,9 @@
 # Inherit mobile full common xdroidOSS stuff
 $(call inherit-product, vendor/xdroid/config/common_mobile.mk)
 
-# Enable support of one-handed mode
+# Enable support of one-handed mode and BT auto-on
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode?=true
+    ro.support_one_handed_mode?=true \
+    bluetooth.server.automatic_turn_on=true
 
 $(call inherit-product, vendor/xdroid/config/telephony.mk)
