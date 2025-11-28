072149e4e49f1097aeed46f1ee74a268bdc41a2a
Nick Reuter <nreuter85@gmail.com>
2025-11-27 21:08:46 -0600

    config: Move device_framework_matrix.xml to hardware/lineage/interfaces
    Follows AOSP structure that exists in hardware/interfaces.
    
    Change-Id: I3ebbd53d4e9920e8a56afd78e18022b06a7b3826

--- config/common.mk
+++ config/common.mk
@@ -74,6 +74,10 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(XDROID_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
+# Lineage interfaces
+PRODUCT_PACKAGES += \
+    framework_compatibility_matrix.lineage.xml
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
