Project-Mica/vendor_mica

mica: Move build-manifest.xml build rule into `build/tasks`
* For Android.mk deprecation

Change-Id: I4aba949ae0af7674955d7a2c439d191348b41f3c

build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/mica/build/core/utils.mk
+
+# Include default installed modules
+include vendor/mica/build/core/default_installed_modules.mk

xdroid-oss/vendor_xdroid

build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/xdroid/build/core/utils.mk
+
+# Include default installed modules
+include vendor/xdroid/build/core/default_installed_modules.mk