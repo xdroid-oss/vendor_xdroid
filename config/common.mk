267b9453a10baf088244412431264f09c49854ae
xyzuan <jodyyuan@xyzuan.com>
2026-04-04 14:27:56 +0000

    config: include vendor_xdroidui

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,7 @@ include vendor/xdroid/config/packages.mk
 include vendor/xdroid/config/version.mk
 include vendor/xdroid/config/opt.mk
 include vendor/xdroid/config/props.mk
+include vendor/xdroidui/config.mk
 
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
