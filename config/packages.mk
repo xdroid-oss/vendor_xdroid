01999c27ba4b36c0a114d71804d258d3d09a37a2
xyzuan <jodyyuan@xyzuan.com>
2026-04-05 04:51:22 +0000

    configs: Update packages config

--- config/packages.mk
+++ config/packages.mk
@@ -1,3 +1,36 @@
+# APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# Binaries
+PRODUCT_PACKAGES += \
+    bash \
+    nano \
+	zstd
+
+PRODUCT_PACKAGES += \
+    nano_recovery
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.ntfs \
+    mount.ntfs
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
+# Camera
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
+
 # Credential storage
 PRODUCT_PACKAGES += \
     android.software.credentials.prebuilt.xml
@@ -9,6 +42,37 @@ PRODUCT_PACKAGES += \
 # Lineage interfaces
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
+
+# Procmem
+PRODUCT_PACKAGES_DEBUG += \
+    procmem
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/procmem
+endif
+
+# Sensitive Phone Numbers list
+PRODUCT_PACKAGES += \
+    sensitive_pn.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    messaging \
+    Stk
+
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/textclassifier/actions_suggestions.universal.model \
+    system/etc/textclassifier/lang_id.model \
+    system/etc/textclassifier/textclassifier.en.model \
+    system/etc/textclassifier/textclassifier.universal.model
 
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
